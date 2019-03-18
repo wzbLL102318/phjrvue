bd9ceb50ee11ee97476f0cb7373550e8c4b6339b
wzb <wzb121318@163.com>
2019-03-18 19:13:14 +0800

    vue commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+   <div class="HelloWorld">
+    <h1>{{ msg }}</h1>
+    <h1>site : {{site}}</h1>
+    <h1>url : {{url}}</h1>
+    <h1>{{details()}}</h1>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'HelloWorld',
+  data () {
+    return {
+      msg: '欢迎来到vue世界！'，
+      site: '菜鸟教程',
+      url: 'www.runoob.com',
+    }
+  },
+    methods: {
+        details: function() {
+        return  this.site + " - 学的不仅是技术，更是梦想！";
+        }
+    }
+}
+</script>
